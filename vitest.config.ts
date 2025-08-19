7a50f2152e4ab85d2d8e2c4e28aff65f2a5e3e85
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-08-19 23:03:53 +0530

    ci: run tests on ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],
     globals: true,
+    include: ['tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['tests/e2e/**/*', 'node_modules/**/*'],
     environmentOptions: {
       jsdom: {
         resources: 'usable',
