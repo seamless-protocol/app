b438893a76fa3747247be7b72c46f56856651d5f
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-09-01 14:31:59 +0530

    chore: cleanup

--- src/features/leverage-tokens/index.ts
+++ src/features/leverage-tokens/index.ts
@@ -4,7 +4,6 @@
  */
 
 // Components will be exported as they're created
-export { MintForm } from './components/MintForm'
 // Export hooks
 export { useMintViaRouter } from './hooks/useMintViaRouter'
 // Export types
