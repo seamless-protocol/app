b214b8a2306830e5c0e756ce6d48f096803d5370
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-09-02 13:20:27 +0530

    ui: add ActivePosition component

---
+++ src/components/icons/arrow-up-right.tsx
@@ -0,0 +1,20 @@
+export function ArrowUpRight({ className = '', size = 24 }: { className?: string; size?: number }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      aria-hidden="true"
+    >
+      <path d="M7 7h10v10" />
+      <path d="M7 17 17 7" />
+    </svg>
+  )
+}
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,10 @@
 // General icons
-
+export { ArrowUpRight } from './arrow-up-right'
+export { Info } from './info'
 // Logo exports
 export * from './logos'
+export { Minus } from './minus'
+export { Plus } from './plus'
 export { SortArrowDown, SortArrowNeutral, SortArrowUp } from './sort-arrows'
+export { USDCLogo, WETHLogo, WeETHLogo } from './token-logos'
 export { WarningIcon } from './warning-icon'
