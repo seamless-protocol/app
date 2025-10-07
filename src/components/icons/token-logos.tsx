fb06959360865151018abca4bf021ec0a85e85a7
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-10-07 11:13:56 +0530

    chore: update seamless logo + remove unsed logo

--- src/components/icons/token-logos.tsx
+++ src/components/icons/token-logos.tsx
@@ -1,5 +1,3 @@
-import { useId } from 'react'
-
 export function USDCLogo({ className = '', size = 32 }: { className?: string; size?: number }) {
   return (
     <svg
@@ -58,41 +56,6 @@ export function WETHLogo({ className = '', size = 32 }: { className?: string; si
           <path d="M16.498 27.995v-6.028L9 17.617z" />
           <path d="M16.498 20.573l7.497-4.353-7.497-3.348z" fillOpacity=".2" />
           <path d="M9 16.22l7.498 4.353v-7.701z" fillOpacity=".602" />
-        </g>
-      </g>
-    </svg>
-  )
-}
-
-export function WeETHLogo({ className = '', size = 32 }: { className?: string; size?: number }) {
-  const gradientId = useId()
-
-  return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 32 32"
-      className={className}
-      xmlns="http://www.w3.org/2000/svg"
-      role="img"
-      aria-label="WeETH Logo"
-    >
-      <title>WeETH Logo</title>
-      <defs>
-        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" stopColor="#8B5CF6" />
-          <stop offset="100%" stopColor="#06B6D4" />
-        </linearGradient>
-      </defs>
-      <g fill="none" fillRule="evenodd">
-        <circle cx="16" cy="16" r="16" fill={`url(#${gradientId})`} />
-        <g fill="#FFF" fillRule="nonzero">
-          <path d="M16.498 4v8.87l7.497 3.35z" fillOpacity=".8" />
-          <path d="M16.498 4L9 16.22l7.498-3.35z" />
-          <path d="M16.498 21.968v6.027L24 17.616z" fillOpacity=".8" />
-          <path d="M16.498 27.995v-6.028L9 17.617z" />
-          <path d="M16.498 20.573l7.497-4.353-7.497-3.348z" fillOpacity=".3" />
-          <path d="M9 16.22l7.498 4.353v-7.701z" fillOpacity=".8" />
         </g>
       </g>
     </svg>
