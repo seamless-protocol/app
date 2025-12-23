4620cf7918b5ccdb884009a0fce7d50fa58d073f
fredwes <6827305+fredwes@users.noreply.github.com>
2025-12-23 13:46:18 -0800

    chore: update wstETH cap (#540)

--- src/features/leverage-tokens/leverageTokens.config.ts
+++ src/features/leverage-tokens/leverageTokens.config.ts
@@ -152,7 +152,7 @@ export const leverageTokenConfigs: Record<string, LeverageTokenConfig> = {
     chainId: 1,
     chainName: 'Ethereum',
     chainLogo: EthereumLogo,
-    supplyCap: 525,
+    supplyCap: 827,
     apyConfig: {
       aprProvider: {
         type: APR_PROVIDERS.DEFI_LLAMA,
