99cd5d7d6d6800e705eee89f593486081b229e6b
fredwes <6827305+fredwes@users.noreply.github.com>
2026-01-08 15:38:54 -0500

    fix: update supplyCap for sUSDS/USDT from 206000 to 252296

--- src/features/leverage-tokens/leverageTokens.config.ts
+++ src/features/leverage-tokens/leverageTokens.config.ts
@@ -693,7 +693,7 @@ export const leverageTokenConfigs: Record<string, LeverageTokenConfig> = {
     chainId: 1,
     chainName: 'Ethereum',
     chainLogo: EthereumLogo,
-    supplyCap: 206000,
+    supplyCap: 252296,
     apyConfig: {
       aprProvider: {
         type: APR_PROVIDERS.DEFI_LLAMA,
