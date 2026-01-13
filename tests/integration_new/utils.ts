91bf02f3286de5688c1d337173de6896977574b8
chad-js <chadflemies@gmail.com>
2026-01-13 14:22:38 -0500

    use separate env var for integration test rpc

--- tests/integration_new/utils.ts
+++ tests/integration_new/utils.ts
@@ -3,10 +3,10 @@ import { base, mainnet } from 'viem/chains'
 
 export const mainnetPublicClient = createPublicClient({
   chain: mainnet,
-  transport: http(import.meta.env['VITE_ETHEREUM_RPC_URL']),
+  transport: http(import.meta.env['VITE_ETHEREUM_FORK_RPC_URL']),
 })
 
 export const basePublicClient = createPublicClient({
   chain: base,
-  transport: http(import.meta.env['VITE_BASE_RPC_URL']),
+  transport: http(import.meta.env['VITE_BASE_FORK_RPC_URL']),
 })
