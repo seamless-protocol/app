3dd90ef58aeeb17c7f1199eacd917df0627297db
chad-js <chadflemies@gmail.com>
2026-01-13 16:01:07 -0500

    use dedicated env vars for fork rpc urls

--- tests/integration_new/setup.ts
+++ tests/integration_new/setup.ts
@@ -24,12 +24,12 @@ globalThis.Request = Request as unknown as typeof globalThis.Request
 
 export const mainnetTest: WagmiChainTestAPI<typeof mainnet> = createWagmiTest(mainnet, {
   forkBlockNumber: await mainnetPublicClient.getBlockNumber(),
-  forkUrl: `https://eth-mainnet.g.alchemy.com/v2/${import.meta.env['VITE_ALCHEMY_API_KEY']}`,
+  forkUrl: import.meta.env['VITE_ETHEREUM_FORK_RPC_URL'],
 })
 
 export const baseTest: WagmiChainTestAPI<typeof base> = createWagmiTest(base, {
   forkBlockNumber: await basePublicClient.getBlockNumber(),
-  forkUrl: `https://base-mainnet.g.alchemy.com/v2/${import.meta.env['VITE_ALCHEMY_API_KEY']}`,
+  forkUrl: import.meta.env['VITE_BASE_FORK_RPC_URL'],
 })
 
 export const wagmiTest = (chainId: number): typeof mainnetTest =>
--- tests/integration_new/utils.ts
+++ tests/integration_new/utils.ts
@@ -3,14 +3,10 @@ import { base, mainnet } from 'viem/chains'
 
 export const mainnetPublicClient = createPublicClient({
   chain: mainnet,
-  transport: http(
-    `https://eth-mainnet.g.alchemy.com/v2/${import.meta.env['VITE_ALCHEMY_API_KEY']}`,
-  ),
+  transport: http(import.meta.env['VITE_ETHEREUM_FORK_RPC_URL']),
 })
 
 export const basePublicClient = createPublicClient({
   chain: base,
-  transport: http(
-    `https://base-mainnet.g.alchemy.com/v2/${import.meta.env['VITE_ALCHEMY_API_KEY']}`,
-  ),
+  transport: http(import.meta.env['VITE_BASE_FORK_RPC_URL']),
 })
